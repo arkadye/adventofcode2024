dd5b07a215fd6e9889e6874fa30f29544a6c274f
arkadye <suicidalduckling@gmail.com>
2024-12-16 23:34:37 +0000

    Some modifications to a_star.h so it sort of works now

--- advent_of_code/utils/a_star.h
+++ advent_of_code/utils/a_star.h
@@ -3,26 +3,33 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-
+#include <concepts>
+#include <type_traits>
+#include <ranges>
 
 #include "advent/advent_assert.h"
 #include "swap_remove.h"
 
-#define ADVENT_A_STAR_USE_UTILS_SORTED_VECTOR 0
+#define ADVENT_A_STAR_USE_UTILS_SORTED_VECTOR 1
 #if ADVENT_A_STAR_USE_UTILS_SORTED_VECTOR
 #include "sorted_vector.h"
 #else
 #include <set>
 #endif
 
+#ifdef NDEBUG
+#define ADVENT_A_STAR_DEBUG_SPAM 0
+#else
+#define ADVENT_A_STAR_DEBUG_SPAM 1
+#endif
+
 namespace utils
 {
 	namespace a_star_internal
 	{
-#define ADVENT_A_STAR_USE_UTILS_SORTED_VECTOR 0
 #if ADVENT_A_STAR_USE_UTILS_SORTED_VECTOR
 		template <typename ValueType, typename Sorter>
-		using NodeSet = utils::small_vector<ValueType, Sorter>;
+		using NodeSet = utils::sorted_vector<ValueType, Sorter>;
 		template <typename ValueType, typename Sorter>
 		void reserve(NodeSet<ValueType, Sorter>& ns, std::size_t num)
 		{
@@ -109,7 +116,7 @@ namespace utils
 
 		struct SortByHeuristic
 		{
-			bool operator()(const AStarNode& l, const AStarNode& r) const noexcept { return l.with_heuristic < r.with_heuristic; }
+			bool operator()(const AStarNode& l, const AStarNode& r) const noexcept { return l.with_heuristic > r.with_heuristic; }
 		};
 
 		ID latest_id = 0;
@@ -132,6 +139,7 @@ namespace utils
 
 		while (!nodes_to_search.empty())
 		{
+#if ADVENT_A_STAR_DEBUG_SPAM
 			std::cout << "\nChecked nodes: ";
 			for (const AStarNode& node : checked_nodes)
 			{
@@ -142,6 +150,7 @@ namespace utils
 			{
 				std::cout << "\n    { " << node.id << ": " << node.cost << '[' << node.with_heuristic << "] " << node.node << " }";
 			}
+#endif
 			AStarNode current_node = get_last(nodes_to_search);
 
 			auto get_path = [&current_node,&get_checked_node_by_id]() -> std::vector<NodeType>
@@ -189,8 +198,12 @@ namespace utils
 				node.node = std::move(n);
 				node.previous_id = current_node.id;
 				node.id = ++latest_id;
-				auto new_it = nodes_to_search.insert(nodes_to_search.upper_bound(node), std::move(node));
-				std::cout << "\nAdding node [ID:" << new_it->id <<  ";C:" << new_it->cost << ";H:" << new_it->with_heuristic << "] " << new_it->node << " to check.";
+#if ADVENT_A_STAR_DEBUG_SPAM
+				std::cout << "\nAdding node [ID:" << node.id << ";C:" << node.cost << ";H:" << node.with_heuristic << "] " << node.node << " to check.";
+#endif
+				nodes_to_search.push_back(std::move(node));
+				//auto new_it = nodes_to_search.insert(nodes_to_search.upper_bound(node), std::move(node));
+				
 			}
 
 			// Update checked nodes
@@ -199,5 +212,36 @@ namespace utils
 
 		// If we run out of nodes, there's no path.
 		return std::make_pair(std::vector<NodeType>{}, CostType{});
+	}
+
+	// NodeType: An arbitrary node. No particular requirements. User provided functors are used to interact.
+	// IsEndPointFunc: A function bool f(Node) that returns true if the argument is an end-point.
+	// GetNextNodesFunc: Return any iterable type containing NodeTypes that can be reached from a NodeType argument.
+	// GetCostBetweenNodesFunc: Functor with the signature: CostType f(NodeType,NodeType).
+	// GetHeuristicForNode: Functor with CostType f(NodeType) to get the heuristic.
+	// AreNodesEqual: A function bool f(NodeType,NodeType) that returns true if both nodes are equal
+	template <
+		typename NodeType,
+		typename IsEndPointFunc,
+		typename GetNextNodesFunc,
+		typename GetCostBetweenNodesFunc,
+		typename GetHeuristicForNode,
+		typename AreNodesEqual,
+		typename LogProgressFunc>
+	auto a_star(
+		const NodeType& start_point,
+		const IsEndPointFunc& is_end_point,
+		const GetNextNodesFunc& get_next_nodes,
+		const GetCostBetweenNodesFunc& get_cost_between_nodes,
+		const GetHeuristicForNode& get_heuristic,
+		const AreNodesEqual& are_nodes_equal,
+		std::size_t estimated_number_of_nodes = 1)
+	{
+		using CostType = decltype(get_cost_between_nodes(start_point, start_point));
+		auto dummy_logger = [](const NodeType& n, CostType cost, CostType cost_with_heuristic, std::size_t processed, std::size_t to_check, const auto& get_nodes)
+			{
+
+			};
+		return a_star(start_point, is_end_point, get_next_nodes, get_cost_between_nodes, get_heuristic, are_nodes_equal, estimated_number_of_nodes, dummy_logger);
 	}
 }
